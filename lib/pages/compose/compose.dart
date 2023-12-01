89581b092335c1e1d1dbc1a14148c518edebb73d
Ambrose Alanda <alandaambrose@gmail.com>
2023-12-01 09:23:47 +0000

    Dec 1, 2023, 12:23 PM

--- lib/pages/compose/compose.dart
+++ lib/pages/compose/compose.dart
@@ -14,23 +14,95 @@ class _ComposeEmailState extends State<ComposeEmail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Compose',
-            style: TextStyle(
-                fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.white,
-          elevation: 0,
-          toolbarHeight: 60,
-          systemOverlayStyle: const SystemUiOverlayStyle(
-            statusBarColor: Colors.black,
-            statusBarIconBrightness: Brightness.light,
-          ),
+      appBar: AppBar(
+        title: const Text(
+          'Compose',
+          style: TextStyle(
+              fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold),
         ),
-        drawer: const CustomDrawer(),
-        body: const Center(
-          child: Text('Compose'),
-        ));
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 60,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.black,
+          statusBarIconBrightness: Brightness.light,
+        ),
+      ),
+      drawer: const CustomDrawer(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //an outline button to show inbox
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/emails');
+              },
+              child: const Text('Inbox'),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'To',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter recipient email address',
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Subject',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter subject',
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Message',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter message',
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Send'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Save'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
