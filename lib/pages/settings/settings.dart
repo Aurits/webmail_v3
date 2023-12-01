36feeb2f03e379e64c5d99911e71c3c2e7d8d8b5
Ambrose Alanda <alandaambrose@gmail.com>
2023-12-01 08:05:50 +0000

    Dec 1, 2023, 11:05 AM

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:settings_ui/settings_ui.dart';
 import '../../widgets/drawer.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -14,23 +14,41 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Settings',
-            style: TextStyle(
-                fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.white,
-          elevation: 0,
-          toolbarHeight: 60,
-          systemOverlayStyle: const SystemUiOverlayStyle(
-            statusBarColor: Colors.black,
-            statusBarIconBrightness: Brightness.light,
-          ),
+      appBar: AppBar(
+        title: const Text(
+          'Settings',
+          style: TextStyle(
+              fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold),
         ),
-        drawer: const CustomDrawer(),
-        body: const Center(
-          child: Text('Settings'),
-        ));
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 60,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.black,
+          statusBarIconBrightness: Brightness.light,
+        ),
+      ),
+      drawer: const CustomDrawer(),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: const Text('Common'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.language),
+                title: const Text('Language'),
+                value: const Text('English'),
+              ),
+              SettingsTile.switchTile(
+                onToggle: (value) {},
+                initialValue: true,
+                leading: const Icon(Icons.format_paint),
+                title: const Text('Enable custom theme'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
