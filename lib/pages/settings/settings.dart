c84eb2a30652be4b6dea05e3702ce0f66cfb5c30
Ambrose Alanda <alandaambrose@gmail.com>
2023-12-01 08:11:10 +0000

    Dec 1, 2023, 11:11 AM

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -30,6 +30,7 @@ class _SettingsPageState extends State<SettingsPage> {
       ),
       drawer: const CustomDrawer(),
       body: SettingsList(
+        key: GlobalKey(debugLabel: 'Settings'),
         sections: [
           SettingsSection(
             title: const Text('Common'),
@@ -44,6 +45,44 @@ class _SettingsPageState extends State<SettingsPage> {
                 title: const Text('Theme'),
                 description: Text('Dark'),
                 onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.switchTile(
+                onToggle: (value) {},
+                initialValue: true,
+                leading: const Icon(Icons.format_paint),
+                title: const Text('Enable custom theme'),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('Common'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.language),
+                title: const Text('Language'),
+                value: const Text('English'),
+              ),
+              SettingsTile(
+                leading: const Icon(Icons.color_lens),
+                title: const Text('Theme'),
+                description: Text('Dark'),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.switchTile(
+                onToggle: (value) {},
+                initialValue: true,
+                leading: const Icon(Icons.format_paint),
+                title: const Text('Enable custom theme'),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('Common'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.language),
+                title: const Text('Language'),
+                value: const Text('English'),
               ),
               SettingsTile.switchTile(
                 onToggle: (value) {},
