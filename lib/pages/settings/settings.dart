73d9933ad63cd7b3de77fd6476cab0116f12b5ac
Ambrose Alanda <alandaambrose@gmail.com>
2023-12-01 08:26:15 +0000

    Dec 1, 2023, 11:26 AM

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -3,14 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:settings_ui/settings_ui.dart';
 import '../../widgets/drawer.dart';
 
-class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
 
-  @override
-  State<SettingsPage> createState() => _SettingsPageState();
-}
-
-class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,98 +24,106 @@ class _SettingsPageState extends State<SettingsPage> {
         ),
       ),
       drawer: const CustomDrawer(),
-      body: SingleChildScrollView(
-        child: SettingsList(
-          key: GlobalKey(debugLabel: 'Settings'),
-          sections: [
-            SettingsSection(
-              title: const Text('Common'),
-              tiles: <SettingsTile>[
-                SettingsTile.switchTile(
-                  onToggle: (value) {},
-                  leading: const Icon(Icons.notifications_active),
-                  title: const Text('Notifications'),
-                  initialValue: true,
-                ),
-                SettingsTile.switchTile(
-                  onToggle: (value) {},
-                  leading: const Icon(Icons.speaker_notes),
-                  title: const Text('Sound'),
-                  initialValue: true,
-                ),
-                SettingsTile.switchTile(
-                  onToggle: (value) {},
-                  leading: const Icon(Icons.vibration),
-                  title: const Text('Vibration'),
-                  initialValue: true,
-                ),
-                SettingsTile.switchTile(
-                  onToggle: (value) {},
-                  leading: const Icon(Icons.airplanemode_active),
-                  title: const Text('Airplane Mode'),
-                  initialValue: true,
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                SettingsList(
+                  key: GlobalKey(debugLabel: 'Settings'),
+                  sections: [
+                    SettingsSection(
+                      title: const Text('Common'),
+                      tiles: <SettingsTile>[
+                        SettingsTile.switchTile(
+                          onToggle: (value) {},
+                          leading: const Icon(Icons.notifications_active),
+                          title: const Text('Notifications'),
+                          initialValue: true,
+                        ),
+                        SettingsTile.switchTile(
+                          onToggle: (value) {},
+                          leading: const Icon(Icons.speaker_notes),
+                          title: const Text('Sound'),
+                          initialValue: true,
+                        ),
+                        SettingsTile.switchTile(
+                          onToggle: (value) {},
+                          leading: const Icon(Icons.vibration),
+                          title: const Text('Vibration'),
+                          initialValue: true,
+                        ),
+                        SettingsTile.switchTile(
+                          onToggle: (value) {},
+                          leading: const Icon(Icons.airplanemode_active),
+                          title: const Text('Airplane Mode'),
+                          initialValue: true,
+                        ),
+                      ],
+                    ),
+                    SettingsSection(
+                      title: const Text('Account'),
+                      tiles: <SettingsTile>[
+                        SettingsTile(
+                          leading: const Icon(Icons.person),
+                          title: const Text('Profile'),
+                          onPressed: (BuildContext context) {},
+                        ),
+                        SettingsTile(
+                          leading: const Icon(Icons.email),
+                          title: const Text('Email'),
+                          onPressed: (BuildContext context) {},
+                        ),
+                        SettingsTile(
+                          leading: const Icon(Icons.phone),
+                          title: const Text('Phone'),
+                          onPressed: (BuildContext context) {},
+                        ),
+                        SettingsTile(
+                          leading: const Icon(Icons.language),
+                          title: const Text('Language'),
+                          onPressed: (BuildContext context) {},
+                        ),
+                        SettingsTile(
+                          leading: const Icon(Icons.help),
+                          title: const Text('Help'),
+                          onPressed: (BuildContext context) {},
+                        ),
+                        SettingsTile(
+                          leading: const Icon(Icons.logout),
+                          title: const Text('Logout'),
+                          onPressed: (BuildContext context) {},
+                        ),
+                      ],
+                    ),
+                    SettingsSection(
+                      title: const Text('Security'),
+                      tiles: <SettingsTile>[
+                        SettingsTile(
+                          leading: const Icon(Icons.lock),
+                          title: const Text('Change Password'),
+                          onPressed: (BuildContext context) {},
+                        ),
+                        SettingsTile.switchTile(
+                          onToggle: (value) {},
+                          leading: const Icon(Icons.fingerprint),
+                          title: const Text('Enable fingerprint'),
+                          initialValue: false,
+                        ),
+                        SettingsTile.switchTile(
+                          onToggle: (value) {},
+                          leading: const Icon(Icons.lock),
+                          title: const Text('Lock app in background'),
+                          initialValue: true,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ],
             ),
-            SettingsSection(
-              title: const Text('Account'),
-              tiles: <SettingsTile>[
-                SettingsTile(
-                  leading: const Icon(Icons.person),
-                  title: const Text('Profile'),
-                  onPressed: (BuildContext context) {},
-                ),
-                SettingsTile(
-                  leading: const Icon(Icons.email),
-                  title: const Text('Email'),
-                  onPressed: (BuildContext context) {},
-                ),
-                SettingsTile(
-                  leading: const Icon(Icons.phone),
-                  title: const Text('Phone'),
-                  onPressed: (BuildContext context) {},
-                ),
-                SettingsTile(
-                  leading: const Icon(Icons.language),
-                  title: const Text('Language'),
-                  onPressed: (BuildContext context) {},
-                ),
-                SettingsTile(
-                  leading: const Icon(Icons.help),
-                  title: const Text('Help'),
-                  onPressed: (BuildContext context) {},
-                ),
-                SettingsTile(
-                  leading: const Icon(Icons.logout),
-                  title: const Text('Logout'),
-                  onPressed: (BuildContext context) {},
-                ),
-              ],
-            ),
-            SettingsSection(
-              title: const Text('Security'),
-              tiles: <SettingsTile>[
-                SettingsTile(
-                  leading: const Icon(Icons.lock),
-                  title: const Text('Change Password'),
-                  onPressed: (BuildContext context) {},
-                ),
-                SettingsTile.switchTile(
-                  onToggle: (value) {},
-                  leading: const Icon(Icons.fingerprint),
-                  title: const Text('Enable fingerprint'),
-                  initialValue: false,
-                ),
-                SettingsTile.switchTile(
-                  onToggle: (value) {},
-                  leading: const Icon(Icons.lock),
-                  title: const Text('Lock app in background'),
-                  initialValue: true,
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
