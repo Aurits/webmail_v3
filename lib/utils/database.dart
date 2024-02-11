b8d432c514d60274e6a45d5c3f67176e4df03d49
Ambrose Alanda <alandaambrose@gmail.com>
2024-02-11 08:57:43 +0000

    Feb 11, 2024, 11:57 AM

--- lib/utils/database.dart
+++ lib/utils/database.dart
@@ -1,8 +1,19 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class Utils {
-  static init() async {
-    var db = openDatabase('local_db.db');
+  static Future<String> getDatabasePath() async {
+    // Get the directory for storing databases
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String dbPath = appDocDir.path + '/local_db.db';
+    return dbPath;
+  }
+
+  static Future<Database> init() async {
+    String dbPath = await getDatabasePath();
+    var db = await openDatabase(dbPath);
     return db;
   }
 }
